98c6eaedac4a2095c83059526736453364d84c41
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2026-01-06 11:53:38 -0500

    #16 simplify style imports

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@import '$lib/styles/variables';`
+				additionalData: `@use '$lib/styles/variables';`
 			}
 		}
 	}
